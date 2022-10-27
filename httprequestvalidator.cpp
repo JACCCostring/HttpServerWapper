ea86605ac0ac89f4d46305fb40d2c522f792d257
JACC <93591202+JACCCostring@users.noreply.github.com>
2022-10-27 09:08:36 +0000

    updated

--- httprequestvalidator.cpp
+++ httprequestvalidator.cpp
@@ -2,10 +2,21 @@
 
 HttpRequestValidator::HttpRequestValidator(const QByteArray &rawRequest)
 {
-    //retrieving const QByteArray to a QString to modify it
-    QString toValidate = rawRequest;
+    //retrieving const QByteArray to a QString to parse it
+    QString forArgs = rawRequest;
+
+    /*
+    NOTE: All the parsing apart from parseRequest() invoke, it's purpuse
+    is to allow access to request route e.x: /var/file/index.html
+                                                    0    1     2 = route 1 / route 2 and route 3
+    but anyway when parseRequest() is invoked route is also parsed but in one line
+    no divided by number of route
+    */
+
+    //parsing http request for method, route, version, key pair values and body
+    parseRequest(rawRequest);
     //splitting by /
-    QStringList splittedRawRequest = toValidate.split("/");
+    QStringList splittedRawRequest = forArgs.split("/");
     //making sure data goes to the right place
     //retrieving num of arguments in request
     int argsNum = getArgsNum(splittedRawRequest);
@@ -20,12 +31,23 @@ HttpRequestValidator::HttpRequestValidator(const QByteArray &rawRequest)
 int HttpRequestValidator::getArgsNum(const QStringList &list)
 {
     int argsNum = 0; //counter init.. to 0
-    for(const QString &lst: qAsConst(list)){
-        if(lst.contains("HTTP")){ break; } //if HTTP tag is found then increment break
+
+    for(const QString &lst: qAsConst(list))
+    {
+        if(lst.contains("HTTP")) { break; } //if HTTP tag is found then break
          argsNum++; //counting every element/args in list
         }
     //returning number of args found in list
     return argsNum;
+}
+
+void HttpRequestValidator::parseBody(const QByteArray &rawRequest)
+{
+        QString findRequestBody = rawRequest;
+        //split string by \r\n\r\n that is at the end of the http request
+        QStringList splittedReq = findRequestBody.split("\r\n\r\n");
+        //retrieving last string - 1 that's the body position in the htttp request
+        request["Body"] = splittedReq[splittedReq.size()-1];
 }
 
 bool HttpRequestValidator::command(const QString command) const
@@ -34,7 +56,7 @@ bool HttpRequestValidator::command(const QString command) const
 }
 
 QStringList HttpRequestValidator::getHttpListArgs()
-{   //method override
+{
     return httpListArgs;
 }
 
@@ -46,4 +68,42 @@ QString HttpRequestValidator::getArgs(int numArg) const
 bool HttpRequestValidator::validateArgs(int numArg) const
 {
     return httpListArgs.size() > numArg;
+}
+
+std::map<QString, QString> HttpRequestValidator::getRequest()
+{
+    return request;
+}
+
+void HttpRequestValidator::parseRequest(const QByteArray &req)
+{
+    QString splitReq = req; //copying to modified
+
+    getMethodPathVersion(req); //parsing method, route and version 1st
+    parseBody(req); //parsing body of request
+
+    QStringList lst = splitReq.split("\n"); //split by \n
+
+    for(const auto &l: qAsConst(lst)){ //looping throug list
+        if(l.contains(":"))
+        { //if its a pair
+            QStringList r = l.split(":"); //then split the pair
+            request[r[0]] = {{r[1]}}; //assigning key and value to a map container
+        }
+    }
+}
+
+void HttpRequestValidator::getMethodPathVersion(const QByteArray &req)
+{
+    QString dataSplitted = req;
+    QStringList list = dataSplitted.split("\n");
+
+    for(const auto &lst: qAsConst(list)){
+        QStringList lstSplitted = lst.split(" ");
+
+        request["Method"] = lstSplitted[0].trimmed(); //appending value without character /n
+        request["Route"] = lstSplitted[1].trimmed();
+        request["Version"] = lstSplitted[2].trimmed();
+        break; //break to avoid overflow
+    }
 }

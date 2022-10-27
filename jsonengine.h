ece3fae6e481f5b6a162da0b0d86dc7dc7b5f836
JACC <93591202+JACCCostring@users.noreply.github.com>
2022-10-27 09:11:41 +0000

    updated

--- jsonengine.h
+++ jsonengine.h
@@ -13,10 +13,12 @@ public:
     explicit JsonEngine();
     //method to add objs to json map
     QByteArray toJson(const QString &, const QVariant &);
+    QVariant fromJson(QByteArray, QString);
     //method to append serialized data to a vector
     void addJsonObj(const QString &, const QVariant &);
     //method to read all json obj from vector obj
     QByteArray readAllJson();
+    void clearJsonVector();
 
 private:
     QVector<QVariantMap> jsonVectorObj;

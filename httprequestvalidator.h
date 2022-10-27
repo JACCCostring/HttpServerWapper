48640e649babcfedfcc9f7f016880628b01c9527
JACC <93591202+JACCCostring@users.noreply.github.com>
2022-10-27 09:07:54 +0000

    updated

--- httprequestvalidator.h
+++ httprequestvalidator.h
@@ -3,6 +3,7 @@
 
 #include <QtCore>
 #include <QTcpSocket>
+
 #include "httprequest.h"
 
 class HttpRequestValidator: public HttpRequest
@@ -17,11 +18,18 @@ public:
     bool validateArgs(int) const;
     //methods to retrieve all arguments used
     QStringList getHttpListArgs();
-    //refactored method to get HTTP commands
+    //get container of key value pair
+    std::map<QString, QString> getRequest();
 
 private:
     //method to count number of arguments in a list of string
     int getArgsNum(const QStringList &);
+    //method to find json body
+    void parseBody(const QByteArray &);
+    //parse request
+    void parseRequest(const QByteArray &);
+    //parse only method, path and version
+    void getMethodPathVersion(const QByteArray &);
 };
 
 #endif // HTTPREQUESTVALIDATOR_H

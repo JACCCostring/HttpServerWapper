a5c7ee2ffadb2b5800e60fc026b8f1ebd96caf46
JACC <93591202+JACCCostring@users.noreply.github.com>
2022-10-27 09:12:39 +0000

    updated

--- jsonengine.cpp
+++ jsonengine.cpp
@@ -11,18 +11,28 @@ QByteArray JsonEngine::toJson(const QString &key, const QVariant &value)
     newJsonObj[key] = value.toString();
 
     if(value.canConvert(QVariant::Int))
-    newJsonObj[key] = value.toString();
+    newJsonObj[key] = value.toInt();
 
     if(value.canConvert(QVariant::Bool))
-    newJsonObj[key] = value.toString();
+    newJsonObj[key] = value.toBool();
 
     if(value.canConvert(QVariant::Double))
-    newJsonObj[key] = value.toString();
+    newJsonObj[key] = value.toDouble();
 
     //setting raw obj to jsonDocument
     jsonDoc.setObject(newJsonObj);
     //returnning obj in  a json format
     return jsonDoc.toJson();
+}
+
+QVariant JsonEngine::fromJson(QByteArray json, QString key)
+{
+    //getting json from request
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(json);
+    //getting obj from QJsonDocument
+    QJsonObject jsonObj = jsonDoc.object();
+    //returning value from jsonObj
+    return jsonObj[key].toVariant();
 }
 
 void JsonEngine::addJsonObj(const QString &key, const QVariant &value)
@@ -57,5 +67,10 @@ QByteArray JsonEngine::readAllJson()
     jsonDoc.setObject(jsonObj);
     //returnning obj in  a json format
     return jsonDoc.toJson();
+}
+
+void JsonEngine::clearJsonVector()
+{
+    jsonVectorObj.clear();
 }
 
